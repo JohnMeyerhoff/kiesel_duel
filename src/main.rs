5733ed42f816b07295740e4e1a8187bb8470fc6a
John <john@meyerhoff.org>
2021-12-20 22:45:43 +0100

    Fixed Mutability
    Todo: Add more i32 to avoid having i8 overflows

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,45 @@
-#[macro_use] extern crate text_io;
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate text_io;
+#[macro_use]
+extern crate rocket;
 
-use serde::{Deserialize};
-use serde::ser::{Serialize, Serializer, SerializeStruct};
+use core::ops::DerefMut;
+use rocket::request::{self, FromRequest, Request};
 use rocket::serde::json::{json, Value};
 use rocket::State;
-use rocket::request::{self, Request, FromRequest};
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+use serde::Deserialize;
 use std::borrow::BorrowMut;
-use core::ops::DerefMut;
+use std::sync::{Arc, Mutex};
 
 struct Stacks {
-     kiesel_a: i8,
-     kiesel_b: i8,
-     winner: i8,
-     a_sub: i8, 
-     b_sub: i8,
-     zug : i8, 
+    lock: Mutex<()>,
+    kiesel_a: i8,
+    kiesel_b: i8,
+    winner: i8,
+    a_sub: i8,
+    b_sub: i8,
+    zug: i32,
 }
 
-impl Stacks{
-    pub fn new() -> Self {
-        let mut kiesel_a: i8 = 13;
-        let mut kiesel_b: i8 = 17;
-        let mut winner: i8= 0;
-        let mut a_sub: i8=0; 
-        let mut b_sub: i8=0;
-        let mut zug = 0;
-
+impl Stacks {
+    pub fn new() -> Stacks {
         Stacks {
-            kiesel_a,  
-            kiesel_b,
-            winner,
-            a_sub, 
-            b_sub,
-            zug,
+            lock: Mutex::new(()),
+            kiesel_a: 13,
+            kiesel_b: 17,
+            winner: 0,
+            a_sub: 0,
+            b_sub: 0,
+            zug: 0,
         }
     }
-    pub fn movel(mut self) {
-        self.zug = self.zug + 1;
+    fn movel(&mut self) {
+        let _lock = self.lock.lock().unwrap();
+        //Held until end of block
+        self.zug += 1;
     }
 }
-
-
 
 impl Serialize for Stacks {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -60,56 +58,62 @@ impl Serialize for Stacks {
     }
 }
 
-
 fn ma0in() {
     println!("\nKieselspiel: Wer den letzten Stein nimmt, verliert.");
     println!("Die Züge werden im Format a b eingegeben,");
     println!("Es dürfen immer nur von einem Stapel beliebig viele,");
     println!("oder von beiden Stapeln gleichviele Steine entfernt werden.");
     let mut status = Stacks::new();
-    while 0<(status.kiesel_a+status.kiesel_b) {
+    while 0 < (status.kiesel_a + status.kiesel_b) {
         if status.zug % 2 == 0 {
             status.winner = 1;
-        }else {
-            status.winner =2;
+        } else {
+            status.winner = 2;
         }
-        println!("Spieler {0} am zug mit A={1} und B={2}:",status.winner,status.kiesel_a,status.kiesel_b);
+        println!(
+            "Spieler {0} am zug mit A={1} und B={2}:",
+            status.winner, status.kiesel_a, status.kiesel_b
+        );
         status.a_sub = read!();
         status.b_sub = read!();
-        if (status.a_sub<=status.kiesel_a && status.b_sub <=status.kiesel_b) && (status.b_sub == status.a_sub || status.b_sub == 0 || status.a_sub == 0) {
+        if (status.a_sub <= status.kiesel_a && status.b_sub <= status.kiesel_b)
+            && (status.b_sub == status.a_sub || status.b_sub == 0 || status.a_sub == 0)
+        {
             status.kiesel_b = status.kiesel_b - status.b_sub;
             status.kiesel_a = status.kiesel_a - status.a_sub;
             status.zug = status.zug + 1;
-        } else{
-         println!("Spieler {0} hat eine ungültige Eingabe getätigt,",(status.zug % 2 +1));
-         println!("es wurden keine Steine entfernt.");
+        } else {
+            println!(
+                "Spieler {0} hat eine ungültige Eingabe getätigt,",
+                (status.zug % 2 + 1)
+            );
+            println!("es wurden keine Steine entfernt.");
         }
     }
-        if status.zug % 2 == 0 {
-            status.winner = 1;
-        }else {
-            status.winner =2;
-        }
+    if status.zug % 2 == 0 {
+        status.winner = 1;
+    } else {
+        status.winner = 2;
+    }
     winsign(status.winner);
 }
 
-fn winsign(player:i8){
-    println!("Spieler {0} hat gewonnen!!!",player);
+fn winsign(player: i8) {
+    println!("Spieler {0} hat gewonnen!!!", player);
 }
 
-fn get_stapel() -> Stacks{
+fn get_stapel() -> Stacks {
     return Stacks::new();
 }
 
-
 #[get("/gamestate?show&<takeaway>")]
-fn gamestate(takeaway:Option<i8>) -> Value {
-    let mut a : Stacks;
+fn gamestate(takeaway: Option<i8>) -> Value {
+    let mut a: Stacks;
     match takeaway {
         Some(b) => {
             a = get_stapel();
-            a.kiesel_a-=b;
-        },
+            a.kiesel_a -= b;
+        }
         None => a = get_stapel(),
     }
     json!(a)
@@ -117,22 +121,19 @@ fn gamestate(takeaway:Option<i8>) -> Value {
 
 #[launch]
 fn rocket() -> _ {
-    let status = Stacks::new();
+    let mut status = Mutex::new(Stacks::new());
     rocket::build()
-    .mount("/", routes![index,gamestate,count])
-    .manage(status)
+        .mount("/", routes![index, gamestate, count])
+        .manage(status)
 }
-
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
-
 }
 #[get("/count")]
-fn count(mut hit_count: &State<Stacks>) -> String {
-    let current_count = hit_count.zug;
-    hit_count.movel();
-    format!("Number of visits: {}", current_count)
+fn count(state: &State<Mutex<Stacks>>) -> String {
+    let mut _lock = state.inner().lock().unwrap();
+    _lock.movel();
+    format!("Number of visits: {}", _lock.zug)
 }
-
